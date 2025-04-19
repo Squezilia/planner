92c540df0bdb527b50734f274a193f31335c13ef
Eren <erendev06@gmail.com>
2025-04-19 12:56:47 +0300

    Project init

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Button } from '../components/ui/button';
+
+</script>
+
+<template>
+  <div class="container flex items-center justify-center h-screen">
+    <Button>
+      Login to Application
+    </Button>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueRouter from "unplugin-vue-router/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [VueRouter({}), vue(), tailwindcss()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
